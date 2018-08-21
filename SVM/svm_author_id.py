9ebaf68457ea9e6c48548ce37573bf5c6f6ed6fd
Shubham Koundinya <skoundinya@uwaterloo.ca>
2018-08-21 14:23:32 -0400

    Add inital Algos

---
+++ SVM/svm_author_id.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(C=10000,kernel='linear')
+t0=time()
+clf.fit(features_train, labels_train)
+print(" Training time",time()-t0)
+t1=time()
+pred= clf.predict(features_test)
+print(" Predicting time",time()-t1)
+print(accuracy_score(pred, labels_test))
+
+
+
+
+#########################################################
+### your code goes here ###
+
+#########################################################
+
+
